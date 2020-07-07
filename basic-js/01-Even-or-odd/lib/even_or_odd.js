1c7a000b7ae5d8a3595101d8b8fc6b2836d60930
unknown <denis.suslov@hotmail.com>
2020-07-07 22:20:33 +0800

    js June 07

---
+++ basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,13 @@
+
+evenOrOdd = (number) => {
+  // TODO: this should return "even" if the number is even, "odd" otherwise
+if (number % 2 == 1) {
+	return console.log('odd')
+}
+else
+
+return console.log('even')
+
+}
+
+evenOrOdd(2)
